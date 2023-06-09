cfd483a8fc41fce18f1b0d5cf174d5e43cd2357f
Baisalya <baishalya1999@gmail.com>
2023-06-09 12:18:33 +0530

    user model

---
+++ api/model/user.js
@@ -0,0 +1,62 @@
+// Import required packages
+const mongoose = require('mongoose');
+
+// Define the schema for regular users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Define the schema for admin users
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin'],
+    default: 'admin',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Define the user model
+const User = mongoose.model('User', userSchema);
+
+// Define the admin model
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = {
+  User,
+  Admin,
+};
